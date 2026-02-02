f1b0eef3462bc22f83dbdf92396fb5739e7f4f2e
Thomas M. Vogel <th-m-vogel@users.noreply.github.com>
2026-02-02 17:55:23 +0100

    refactor, uptdate in function

--- ED_BioAlert.ps1
+++ ED_BioAlert.ps1
@@ -46,6 +46,110 @@ Function New-EDMessage {
             
 }
 
+Function New-Event {
+
+    ### write event types to console
+    Write-Host $line.event
+
+    ###
+    # evaluation shit happens here
+    ###
+    $updated = $false
+            
+    ### Listen to Events we are interested in
+
+    # need to care about scanned systems - event":"FSSDiscoveryScan", "Progress":1.000000 }
+
+            
+    ### StartJump - clear system Data on FSD Jump
+    if ($line.event -eq "StartJump" -and $Starsystem.Count -gt 0) {
+                
+        New-EDMessage -Voice $Lifescan -Message "System jump detected, safe system data to disk"
+        ## Dump System to Disk
+        $fixed = @{} 
+        foreach ($key in $Starsystem.Keys) { $fixed["$key"] = $Starsystem[$key] } $fixed | ConvertTo-Json | Set-Content "$LogPath\SystemData\$($Starsystem[$key].StarSystem).json"
+    }
+    ### FSDJump - read exiting system data if exist
+    if ($line.event -eq "FSDJump") {
+        ## clear Data
+        New-EDMessage -Voice $Lifescan -Message "System jump finished"
+        $Starsystem = @{}
+        ## Read System from Disk
+        if (Test-Path "$LogPath\SystemData\$($Line.StarSystem).json") { 
+            New-EDMessage -Voice $Lifescan -Message "Load system information for $($Line.StarSystem)"
+            # File exists → import JSON into a PSObject with integer keys 
+            $Data = Get-Content $LogPath\SystemData\$($Line.StarSystem).json -Raw | ConvertFrom-Json 
+            $Starsystem = @{} 
+            foreach ($key in $Data.PSObject.Properties.Name) { 
+                $intKey = [int]$key 
+                $Starsystem[$intKey] = $Data.$key 
+            }
+            Write-Host "##### JSON file loaded for $($Line.StarSystem)" 
+            $Starsystem | Format-Table
+        }
+    }
+
+
+    ### Scan Events
+    if ($line.event -eq "Scan" -or $line.event-eq "ScanBaryCentre") {
+        $updated = $true
+        if ( $Starsystem[$line.BodyID] -eq $null ) {
+            $Starsystem[$line.BodyID] += $line
+        } else {
+            foreach ($prop in $line.PSObject.Properties) { 
+                    $Starsystem[$line.BodyID] | Add-Member -MemberType NoteProperty -Name $prop.Name -Value $prop.Value -Force 
+            }
+        }
+        ###
+        if ($debug) {New-EDMessage -Voice $Lifescan -Message "Detected Scan type $($line.ScanType) for body number $($line.BodyID). Starsystem has now $($Starsystem.Count) members"}
+        # $Starsystem[$line.BodyID] | Format-List
+
+    }
+    ### FSS / SAA Events
+    if (($line.event -eq "FSSBodySignals") -or ($line.event -eq "SAASignalsFound")) {
+        $updated = $true
+        if ( $Starsystem[$line.BodyID] -eq $null ) {
+            $Starsystem[$line.BodyID] += $line
+        } else {
+            $Starsystem[$line.BodyID] | Add-Member -MemberType NoteProperty -Name Signals -Value $line.Signals -Force
+        }
+        if ($debug) {New-EDMessage -Voice $Lifescan -Message "FSS or SAA signals detected for body numer $($line.BodyID). Starsystem has now $($Starsystem.Count) members"}
+                
+    }
+            
+
+    ###
+    # Evaluation Part
+    ###
+    if ($updated) {
+        if ($Starsystem[$line.BodyID].Signals) {
+            ### Shout out HMC with Signals
+            If ($Starsystem[$line.BodyID].PlanetClass -eq "High metal content body") {
+                New-EDMessage -Voice $Lifescan -Message "High metal content body with signals found"
+                $Starsystem[$line.BodyID].Signals | Format-Table
+            }
+        }
+    }
+
+    ###
+    # evaluation finished
+    ###
+            
+    ###
+    # Finished FSS detected / discovered System 
+    ###
+    if ($line.event -eq "FSSAllBodiesFound" -or ($line.event -eq "FSSDiscoveryScan" -and $line.Progress -eq 1 )) {
+        New-EDMessage -Voice $Lifescan -Message "Finished FSS Scan detected, found $($Starsystem.Count) system members"
+
+        # Log Console Bodies with Signals
+        foreach ($Key in $Starsystem.keys) {
+            if ($Starsystem[$Key].Signals -ne $null) {
+                Write-Host $Starsystem[$Key].BodyName $Starsystem[$Key].Signals
+            }
+        }
+    }
+}
+
 while ($true) {
 
     # Detect newest file
@@ -83,106 +187,7 @@ while ($true) {
 
         while (-not $reader.EndOfStream) {
             $line = $reader.ReadLine() | ConvertFrom-Json
-            
-            ### write event types to console
-            Write-Host $line.event
-
-            ###
-            # evaluation shit happens here
-            ###
-            $updated = $false
-            
-            ### Listen to Events we are interested in
-
-            # need to care about scanned systems - event":"FSSDiscoveryScan", "Progress":1.000000 }
-
-            
-            ### StartJump - clear system Data on FSD Jump
-            if ($line.event -eq "StartJump") {
-                
-                New-EDMessage -Voice $Lifescan -Message "System jump detected, safe system data to disk"
-                ## Dump System to Disk
-                $fixed = @{} 
-                foreach ($key in $Starsystem.Keys) { $fixed["$key"] = $Starsystem[$key] } $fixed | ConvertTo-Json | Set-Content "$LogPath\SystemData\$($Starsystem[$key].StarSystem).json"
-            }
-            ### FSDJump - read exiting system data if exist
-            if ($line.event -eq "FSDJump") {
-                ## clear Data
-                $Starsystem = @{}
-                ## Read System from Disk
-                if (Test-Path "$LogPath\SystemData\$($Line.StarSystem).json") { 
-                    # File exists → import JSON into a PSObject with integer keys 
-                    $Data = Get-Content $LogPath\SystemData\$($Line.StarSystem).json -Raw | ConvertFrom-Json 
-                    $Starsystem = @{} 
-                    foreach ($key in $Data.PSObject.Properties.Name) { 
-                        $intKey = [int]$key 
-                        $Starsystem[$intKey] = $Data.$key 
-                    }
-                    Write-Host "##### JSON file loaded for $($Line.StarSystem)" 
-
-                    $Starsystem | Format-Table
-                }
-            }
-
-
-            ### Scan Events
-            if ($line.event -eq "Scan" -or $line.event-eq "ScanBaryCentre") {
-                $updated = $true
-                if ( $Starsystem[$line.BodyID] -eq $null ) {
-                    $Starsystem[$line.BodyID] += $line
-                } else {
-                    foreach ($prop in $line.PSObject.Properties) { 
-                         $Starsystem[$line.BodyID] | Add-Member -MemberType NoteProperty -Name $prop.Name -Value $prop.Value -Force 
-                    }
-                }
-                ###
-                if ($debug) {New-EDMessage -Voice $Lifescan -Message "Detected Scan type $($line.ScanType) for body number $($line.BodyID). Starsystem has now $($Starsystem.Count) members"}
-                # $Starsystem[$line.BodyID] | Format-List
-
-            }
-            ### FSS / SAA Events
-            if (($line.event -eq "FSSBodySignals") -or ($line.event -eq "SAASignalsFound")) {
-                $updated = $true
-                if ( $Starsystem[$line.BodyID] -eq $null ) {
-                    $Starsystem[$line.BodyID] += $line
-                } else {
-                    $Starsystem[$line.BodyID] | Add-Member -MemberType NoteProperty -Name Signals -Value $line.Signals -Force
-                }
-                if ($debug) {New-EDMessage -Voice $Lifescan -Message "FSS or SAA signals detected for body numer $($line.BodyID). Starsystem has now $($Starsystem.Count) members"}
-                
-            }
-            
-
-            ###
-            # Evaluation Part
-            ###
-            if ($updated) {
-               if ($Starsystem[$line.BodyID].Signals) {
-                    ### Shout out HMC with Signals
-                    If ($Starsystem[$line.BodyID].PlanetClass -eq "High metal content body") {
-                        New-EDMessage -Voice $Lifescan -Message "High metal content body with signals found"
-                        $Starsystem[$line.BodyID].Signals | Format-Table
-                    }
-                }
-            }
-
-            ###
-            # evaluation finished
-            ###
-            
-            ###
-            # Finished FSS detected / discovered System 
-            ###
-            if ($line.event -eq "FSSAllBodiesFound" -or ($line.event -eq "FSSDiscoveryScan" -and $line.Progress -eq 1 )) {
-                New-EDMessage -Voice $Lifescan -Message "Finished FSS Scan detected, found $($Starsystem.Count) system members"
-
-                # Log Console Bodies with Signals
-                foreach ($Key in $Starsystem.keys) {
-                    if ($Starsystem[$Key].Signals -ne $null) {
-                        Write-Host $Starsystem[$Key].BodyName $Starsystem[$Key].Signals
-                    }
-                }
-            }
+            New-Event
         }
 
         $lastLength = $currentStream.Position
