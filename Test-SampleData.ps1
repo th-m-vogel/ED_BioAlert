26631ba86ea5f3cc635e52d3ab822d5cb41143cc
Thomas M. Vogel <th-m-vogel@users.noreply.github.com>
2026-03-23 11:16:50 +0000

    Remove Sample Data folder and Test-SampleData.ps1 - superseded by -TestFile parameter and full journal availability

--- Test-SampleData.ps1
+++
@@ -1,6 +0,0 @@
-#############################################################################
-# Test script - runs log import against journal data on Linux
-#############################################################################
-
-$LogPath = Join-Path $PSScriptRoot "..\Journal"
-. (Join-Path $PSScriptRoot "ED_BioAlert.ps1") -ScanAll
